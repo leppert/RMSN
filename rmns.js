21f1be3a951215f2b2f0d673831a9ceec871e603
Greg Leppert <greg@formasfunction.com>
2011-10-30 14:58:50 -0400

    Hello world

--- rmns.js
+++ rmns.js
@@ -1,1 +1,11 @@
+var server = require('http').createServer(function(req, res){
+  res.end('hello world');
+});
+server.listen(8080);
+
 var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, message);
+};
