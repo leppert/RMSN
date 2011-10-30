3dd51780c1c15787a65fbbd87b8eb0e36aaec260
Greg Leppert <greg@formasfunction.com>
2011-10-30 16:35:54 -0400

    Basic POST working

--- rmns.js
+++ rmns.js
@@ -1,22 +1,40 @@
-var fs = require('fs');
-var express = require('express');
-var nowjs = require("now");
+// Initialize libs
+var fs      = require('fs'),
+    express = require('express'),
+    nowjs   = require('now');
 
+//###############//
+// -- Express -- //
+//###############//
 var app = express.createServer(express.logger());
+// Parse the POST data when it comes in
+app.use(express.bodyParser());
+// Start listening for requests
+var port = process.env.PORT || 3000;
+app.listen(port, function(){ console.log("Listening on " + port); });
 
+//##############//
+// -- Now.js -- //
+//##############//
+var everyone = nowjs.initialize(app);
+
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, message);
+};
+
+//###########//
+// -- GET -- //
+//###########//
 app.get('/', function(request, response) {
   fs.readFile(__dirname + '/helloworld.html', 'utf8', function(err, text){
     response.send(text);
   });
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+//############//
+// -- POST -- //
+//############//
+app.post('/', function(request, response){
+  everyone.now.receiveMessage('test', request.body.message);
+  response.send('true');
 });
-
-var everyone = nowjs.initialize(app);
-
-everyone.now.distributeMessage = function(message){
-  everyone.now.receiveMessage(this.now.name, message);
-};
