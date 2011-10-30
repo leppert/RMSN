e35411e2a038d59ede9d003b450e7b8717c34293
Greg Leppert <greg@formasfunction.com>
2011-10-30 15:20:17 -0400

    Adding Express to hello world

--- rmns.js
+++ rmns.js
@@ -1,11 +1,21 @@
-var server = require('http').createServer(function(req, res){
-  res.end('hello world');
-});
-var port = process.env.PORT || 8080;
-server.listen(port);
-
+var fs = require('fs');
+var express = require('express');
 var nowjs = require("now");
-var everyone = nowjs.initialize(server);
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  fs.readFile(__dirname + '/helloworld.html', 'utf8', function(err, text){
+    response.send(text);
+  });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+var everyone = nowjs.initialize(app);
 
 everyone.now.distributeMessage = function(message){
   everyone.now.receiveMessage(this.now.name, message);
