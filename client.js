bf501f6d671638d4fcb30533eff54303a7f2e11a
Greg Leppert <greg@formasfunction.com>
2011-10-30 22:53:42 -0400

    Working on aliasing Pusher

--- client.js
+++ client.js
@@ -1,1 +1,130 @@
-This is a static file
+/*!
+ * RMSN JavaScript Library v0.0.1
+ * http://pusherapp.com/
+ *
+ * Copyright 2011, Greg Leppert
+ * Released under the MIT licence.
+ */
+
+var RMSN = function(app_key, options){
+};
+
+RMSN.prototype = {
+  channel: function(name) {
+  },
+
+  connect: function() {
+  },
+
+  disconnect: function() {
+  },
+
+  bind: function(event_name, callback) {
+  },
+
+  bind_all: function(callback) {
+  },
+
+  subscribeAll: function() {
+  },
+
+  subscribe: function(channel_name) {
+  },
+
+  unsubscribe: function(channel_name) {
+  },
+
+  send_event: function(event_name, data, channel) {
+  },
+
+  send_local_event: function(event_name, event_data, channel_name) {
+  }
+};
+
+RMSN.Channels = function() {
+  this.channels = {};
+};
+
+RMSN.Channels.prototype = {
+  add: function(channel_name, pusher) {
+  },
+
+  find: function(channel_name) {
+  },
+
+  remove: function(channel_name) {
+  },
+
+  disconnect: function () {
+  }
+};
+
+RMSN.Channel = function(channel_name, pusher) {
+};
+
+RMSN.Channel.prototype = {
+  disconnect: function(){
+  },
+
+  acknowledge_subscription: function(data){
+  },
+
+  is_private: function(){
+  },
+
+  is_presence: function(){
+  },
+
+  authorize: function(pusher, callback){
+  },
+
+  trigger: function(event, data) {
+  }
+};
+
+RMSN.Channel.PrivateChannel = {
+  is_private: function(){
+  },
+
+  authorize: function(pusher, callback){
+  }
+};
+
+RMSN.Channel.PresenceChannel = {
+
+  init: function(){
+  },
+
+  disconnect: function(){
+  },
+
+  acknowledge_subscription: function(sub_data){
+  },
+
+  is_presence: function(){
+  },
+
+  members: {
+    _members_map: {},
+    count: 0,
+
+    each: function(callback) {
+    },
+
+    add: function(id, info) {
+    },
+
+    remove: function(user_id) {
+
+    },
+
+    get: function(user_id) {
+    },
+
+    clear: function() {
+    }
+  }
+};
+
+// Alias to Pusher
+var Pusher = RMSN;
